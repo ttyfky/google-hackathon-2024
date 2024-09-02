145aba46f1aa313244cb94a1e5ae34f06d7eb4df
kobori <kobori-t@belong.co.jp>
2024-09-02 14:10:26 +0900

    #28 remove redundant import

--- b_moz/libs/retreivers/google_search.py
+++ b_moz/libs/retreivers/google_search.py
@@ -1,5 +1,5 @@
 import logging
-from typing import List, Optional
+from typing import List
 
 from langchain_core.callbacks.manager import CallbackManagerForRetrieverRun
 from langchain_core.documents import Document
