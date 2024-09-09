f83382a7f625adcc56c7e90ed0ee38de4da5332e
kobori <kobori-t@belong.co.jp>
2024-09-09 18:29:11 +0900

    #31 skip pdf fetch, add num_result parameter to GoogleSearchJsonResultRetriever

--- b_moz/libs/retreivers/google_search.py
+++ b_moz/libs/retreivers/google_search.py
@@ -8,7 +8,7 @@ from langchain_google_community import GoogleSearchAPIWrapper
 
 from b_moz.libs.document_loaders.custom_bs_loader import CustomBSHTMLLoader
 
-num_results = 3
+MAX_RESULTS = 10
 _logger = logging.getLogger(__name__)
 
 
@@ -17,23 +17,31 @@ class GoogleSearchJsonResultRetriever(BaseRetriever):
 
     query_tmpl: str
     as_html: bool
+    num_results: int
 
-    def __init__(self, query_tmpl: str, as_html: bool = False):
-        super().__init__(query_tmpl=query_tmpl, as_html=as_html)  # type: ignore
+    def __init__(self, query_tmpl: str, as_html: bool = False, num_results: int = 3):
+        super().__init__(query_tmpl=query_tmpl, as_html=as_html, num_results=num_results)  # type: ignore
 
     def _get_relevant_documents(
         self, query: str, *, run_manager: CallbackManagerForRetrieverRun
     ) -> List[Document]:
         search = GoogleSearchAPIWrapper()
         results = search.results(
-            query=self.query_tmpl.format(query=query), num_results=num_results
+            query=self.query_tmpl.format(query=query), num_results=MAX_RESULTS
         )
         docs = []
         for r in results:
+            link = r["link"]
+            if link.endswith(".pdf"):
+                # some pdf causes invalid augument error
+                _logger.info(f"Skip to fetch pdf: {link}")
+                continue
             try:
-                docs.append(self._fetch_as_document(r["link"]))
+                docs.append(self._fetch_as_document(link))
+                if len(docs) >= self.num_results:
+                    break
             except Exception as e:
-                _logger.warning(f"Failed to fetch {r['link']} with error: {e}")
+                _logger.warning(f"Failed to fetch {link} with error: {e}")
                 continue
         return docs
 
